a298a9b31f21aed6bc57a49fd08702f3ff1f9360
Pradiss <erick.p436@gmail.com>
2025-07-05 14:44:53 -0300

    mask

---
+++ src/mask/mascara.js
@@ -0,0 +1,27 @@
+export const formatPhone = (value) => {
+    const cleaned = value.replace(/\D/g, "").slice(0, 11);
+    if (cleaned.length <= 10) {
+      return cleaned
+        .replace(/(\d{2})(\d)/, "($1) $2")
+        .replace(/(\d{4})(\d)/, "$1-$2");
+    } else {
+      return cleaned
+        .replace(/(\d{2})(\d)/, "($1) $2")
+        .replace(/(\d{5})(\d)/, "$1-$2");
+    }
+  };
+ export const formatCPF = (value) =>
+    value
+      .replace(/\D/g, "") // só dígitos
+      .replace(/(\d{3})(\d)/, "$1.$2") // 123.456
+      .replace(/(\d{3})\.(\d{3})(\d)/, "$1.$2.$3") // 123.456.789
+      .replace(/(\d{3})\.(\d{3})\.(\d{3})(\d)/, "$1.$2.$3-$4") // 123.456.789-00
+      .slice(0, 14);
+
+export  const formatCEP = (value = "") => {
+    return value
+      .replace(/\D/g, "") // Remove tudo que não for número
+      .slice(0, 8) // Limita a 8 dígitos
+      .replace(/^(\d{5})(\d)/, "$1-$2"); // Insere o hífen entre o 5º e 6º dígito
+  };
+
--- src/pages/FormRegister.js
+++ src/pages/FormRegister.js
@@ -14,6 +14,7 @@ import {
 } from "react-native";
 import styles from "../components/Style";
 import { Button } from "react-native-paper";
+import { formatCEP,formatCPF,formatPhone } from "../mask/mascara";
 
 export default function FormRegister({ navigation }) {
   const [email, setEmail] = useState([]);
@@ -105,32 +106,32 @@ export default function FormRegister({ navigation }) {
     Alert.alert("Formulario enviado com sucesso ");
   };
 
-  const formatPhone = (value) => {
-    const cleaned = value.replace(/\D/g, "").slice(0, 11);
-    if (cleaned.length <= 10) {
-      return cleaned
-        .replace(/(\d{2})(\d)/, "($1) $2")
-        .replace(/(\d{4})(\d)/, "$1-$2");
-    } else {
-      return cleaned
-        .replace(/(\d{2})(\d)/, "($1) $2")
-        .replace(/(\d{5})(\d)/, "$1-$2");
-    }
-  };
-  const formatCPF = (value) =>
-    value
-      .replace(/\D/g, "") // só dígitos
-      .replace(/(\d{3})(\d)/, "$1.$2") // 123.456
-      .replace(/(\d{3})\.(\d{3})(\d)/, "$1.$2.$3") // 123.456.789
-      .replace(/(\d{3})\.(\d{3})\.(\d{3})(\d)/, "$1.$2.$3-$4") // 123.456.789-00
-      .slice(0, 14);
+  // const formatPhone = (value) => {
+  //   const cleaned = value.replace(/\D/g, "").slice(0, 11);
+  //   if (cleaned.length <= 10) {
+  //     return cleaned
+  //       .replace(/(\d{2})(\d)/, "($1) $2")
+  //       .replace(/(\d{4})(\d)/, "$1-$2");
+  //   } else {
+  //     return cleaned
+  //       .replace(/(\d{2})(\d)/, "($1) $2")
+  //       .replace(/(\d{5})(\d)/, "$1-$2");
+  //   }
+  // };
+  // const formatCPF = (value) =>
+  //   value
+  //     .replace(/\D/g, "") // só dígitos
+  //     .replace(/(\d{3})(\d)/, "$1.$2") // 123.456
+  //     .replace(/(\d{3})\.(\d{3})(\d)/, "$1.$2.$3") // 123.456.789
+  //     .replace(/(\d{3})\.(\d{3})\.(\d{3})(\d)/, "$1.$2.$3-$4") // 123.456.789-00
+  //     .slice(0, 14);
 
-  const formatCEP = (value = "") => {
-    return value
-      .replace(/\D/g, "") // Remove tudo que não for número
-      .slice(0, 8) // Limita a 8 dígitos
-      .replace(/^(\d{5})(\d)/, "$1-$2"); // Insere o hífen entre o 5º e 6º dígito
-  };
+  // const formatCEP = (value = "") => {
+  //   return value
+  //     .replace(/\D/g, "") // Remove tudo que não for número
+  //     .slice(0, 8) // Limita a 8 dígitos
+  //     .replace(/^(\d{5})(\d)/, "$1-$2"); // Insere o hífen entre o 5º e 6º dígito
+  // };
   const steps = () => {
     switch (step) {
       case 1:
